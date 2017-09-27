dbb28c2ac6e2d4d906f4a1589a6f397341d2bbec
Asher Guan <>
2017-09-28 01:11:38 +0800

    emacs config

---
+++ configuration/emacs/elpa/yasnippet-20170923.1646/yasnippet-autoloads.el
@@ -0,0 +1,15 @@
+;;; yasnippet-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("yasnippet-pkg.el") (22984 29837 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; yasnippet-autoloads.el ends here
