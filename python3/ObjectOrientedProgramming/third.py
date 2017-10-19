6402e90ce2a8f2aaf7e3947ed3bca0df791827cd
Asher Guan <>
2017-10-19 22:53:22 +0800

    contact inherit supplier

--- python3/ObjectOrientedProgramming/third.py
+++ python3/ObjectOrientedProgramming/third.py
@@ -1,3 +1,28 @@
 # inheriting from object
 class SubClass(object):
     pass
+
+
+# simple contact class to inherit from
+class Contact:
+    all_contacts = []  # global variable
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)  # the way call the all_contacts
+
+
+# contact inherit supplier
+class Supplier(Contact):
+    def order(self, order):
+        print("If this were a real system we would send"
+              "{} order to {}".format(order, self.name))
+
+
+
+
+
+
+
+
