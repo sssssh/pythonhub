ec01f53e11728e8be63f16bfe5d50728a6e45207
Asher Guan <>
2017-10-20 17:14:29 +0800

    inventory handling

--- python3/ObjectOrientedProgramming/fouth.py
+++ python3/ObjectOrientedProgramming/fouth.py
@@ -189,3 +189,28 @@ class Inventory:
         is available, subtract one item and return
         the number of items left.'''
         pass
+
+
+# handling inventory
+class InvalidItemType:
+    pass
+
+
+class OutOfStock:
+    pass
+
+
+item_type = 'widget'
+inv = Inventory()
+inv.lock(item_type)
+try:
+    num_left = inv.purchase(item_type)
+except InvalidItemType:
+    print("Sorry, we don't sell {}".format(item_type))
+except OutOfStock:
+    print("Sorry, that item is out of stock.")
+else:
+    print("Purchase complete. There are "
+          "{} {}s left".format(num_left, item_type))
+finally:
+    inv.unlock()
