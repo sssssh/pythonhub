a32e22325ad11e454114e796d0ca309ca4de6a80
Asher Guan <>
2017-10-20 17:20:59 +0800

    login

--- python3/ObjectOrientedProgramming/fouth.py
+++ python3/ObjectOrientedProgramming/fouth.py
@@ -254,6 +254,14 @@ class PasswordTooShort(AuthException):
     pass
 
 
+class InvalidUsername(AuthException):
+    pass
+
+
+class InvalidPassword(AuthException):
+    pass
+
+
 # authenticator
 class Authenticator:
     def __init__(self):
@@ -267,3 +275,20 @@ class Authenticator:
         if len(password) < 6:
             raise PasswordTooShort(username)
         self.users[username] = User(username, password)
+
+    def login(self, username, password):
+        try:
+            user = self.users[username]
+        except KeyError:
+            raise InvalidUsername(username)
+
+        if not user.check_password(password):
+            raise InvalidPassword(username, user)
+
+        user.is_logged_in = True
+        return True
+
+    def is_logged_in(self, username):
+        if username in self.users:
+            return self.users[username].is_logged_in
+        return False
