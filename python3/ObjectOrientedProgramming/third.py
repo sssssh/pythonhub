73c2eeb2cd8cff98e474ed75530fde57f1cdb346
Asher Guan <>
2017-10-20 16:07:32 +0800

    type map

--- python3/ObjectOrientedProgramming/third.py
+++ python3/ObjectOrientedProgramming/third.py
@@ -513,3 +513,20 @@ class Agent:
     def display_properties():
         for property in self.property_list:
             property.display()
+
+
+# type map
+class Agent2:
+    type_map = {
+        ("house", "rental"): HouseRental,
+        ("house", "purchase"): HousePurchase,
+        ("apartment", "rental"): ApartmentRental,
+        ("apartment", "purchase"): ApartmentPurchase
+        }
+
+    def __init__(self):
+        self.property_list = []
+
+    def display_properties():
+        for property in self.property_list:
+            property.display()
