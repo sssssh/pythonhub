713e75cae9b6768d464744e83b580d153522c7cb
Asher Guan <>
2017-10-20 14:45:18 +0800

    polymorphic - audio

--- python3/ObjectOrientedProgramming/third.py
+++ python3/ObjectOrientedProgramming/third.py
@@ -199,3 +199,33 @@ class Friend3(Contact2, AddressHolder1):
         print(kwargs)
         super().__init__(**kwargs)
         self.phone = phone
+
+
+# polymorphic audio
+class AudioFile:
+    def __init__(self, filename):
+        if not filename.endswith(self.ext):
+            raise Exception("Invalid file format")
+
+        self.filename = filename
+
+
+class MP3File(AudioFile):
+    ext = "mp3"
+
+    def play(self):
+        print("playing {} as mp3".format(self.filename))
+
+
+class WavFile(AudioFile):
+    ext = "wav"
+
+    def play(self):
+        print("playing {} as wav".format(self.filename))
+
+
+class OggFile(AudioFile):
+    ext = "ogg"
+
+    def play(self):
+        print("playing {} as ogg".format(self.filename))
