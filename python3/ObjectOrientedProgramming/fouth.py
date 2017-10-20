84338fca421ed7500a90b4946906909514f7db7e
Asher Guan <>
2017-10-20 17:10:16 +0800

    inventory mock object

--- python3/ObjectOrientedProgramming/fouth.py
+++ python3/ObjectOrientedProgramming/fouth.py
@@ -163,3 +163,29 @@ divide_with_exception(10, 5)
 divide_with_if(10, 5)
 divide_with_if(10, 0)
 divide_with_exception(10, 0)
+
+
+# inventory mock object
+class Inventory:
+    def lock(self, item_type):
+        '''Select the type of item that is going to
+        be manipulated. This method will lock the
+        item so nobody else can manipulate the
+        inventory until it's returned. This prevents
+        selling the same item to two different
+        customers.'''
+        pass
+
+    def unlock(self, item_type):
+        '''Release the given type so that other
+        customers can access it.'''
+        pass
+
+    def purchase(self, item_type):
+        '''If the item is not locked, raise an
+        exception. If the itemtype does not exist,
+        raise an exception. If the item is currently
+        out of stock, raise an exception. If the item
+        is available, subtract one item and return
+        the number of items left.'''
+        pass
