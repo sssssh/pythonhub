6e77014c8b565d736b546af6a754cae08ccbc74f
Asher Guan <>
2017-10-20 15:40:50 +0800

    validation function

--- python3/ObjectOrientedProgramming/third.py
+++ python3/ObjectOrientedProgramming/third.py
@@ -345,3 +345,12 @@ class Apartment(Property):
         })
         return parent_init
     prompt_init = staticmethod(prompt_init)
+
+
+# validation function
+def get_valid_input(input_string, valid_options):
+    input_string += " ({}) ".format(", ".join(valid_options))
+    response = input(input_string)
+    while response.lower() not in valid_options:
+        response = input(input_string)
+    return response
