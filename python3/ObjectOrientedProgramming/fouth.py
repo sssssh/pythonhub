ffcb30ab0a1f9d07cc88dd8a06fd1ec4258c4b0c
Asher Guan <>
2017-10-20 16:51:01 +0800

    finally and else

--- python3/ObjectOrientedProgramming/fouth.py
+++ python3/ObjectOrientedProgramming/fouth.py
@@ -1,3 +1,6 @@
+import random
+
+
 # even integers
 class EvenOnly(list):
     def append(self, integer):
@@ -85,3 +88,23 @@ try:
     raise ValueError("This is an argument")
 except ValueError as e:
     print("The exception arguments were", e.args)
+
+
+# finally and else
+some_exceptions = [ValueError, TypeError, IndexError, None]
+
+try:
+    choice = random.choice(some_exceptions)
+    print("raising {}".format(choice))
+    if choice:
+        raise choice("An error")
+except ValueError:
+    print("Caught a ValueError")
+except TypeError:
+    print("Caught a TypeError")
+except Exception as e:
+    print("Caught some other error: %s" % e.__class__.__name__)
+else:
+    print("This code called if there is no exception")
+finally:
+    print("This cleanup code is always called")
