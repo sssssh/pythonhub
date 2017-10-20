ff391e567041c822dfbd8ea34a3c86a24596fef2
Asher Guan <>
2017-10-20 16:45:44 +0800

    catch specific exception

--- python3/ObjectOrientedProgramming/fouth.py
+++ python3/ObjectOrientedProgramming/fouth.py
@@ -30,3 +30,16 @@ try:
 except:
     print("I caught an exception")
 print("executed after the exception")
+
+
+# catch specific exception
+def funny_division(anumber):
+    try:
+        return 100 / anumber
+    except ZeroDivisionError:
+        return "Silly wabbit, you can't divide by zero!"
+
+
+print(funny_division(0))
+print(funny_division(50.0))
+print(funny_division("hello"))
