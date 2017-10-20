b88a69c26fb43ba683e77d0bbc4fb3deb00564ea
Asher Guan <>
2017-10-20 17:16:35 +0800

    auth user

--- python3/ObjectOrientedProgramming/fouth.py
+++ python3/ObjectOrientedProgramming/fouth.py
@@ -1,4 +1,5 @@
 import random
+import hashlib
 
 
 # even integers
@@ -214,3 +215,40 @@ else:
           "{} {}s left".format(num_left, item_type))
 finally:
     inv.unlock()
+
+
+class User:
+    def __init__(self, username, password):
+        '''Create a new user object. The password
+        will be encrypted before storing.'''
+        self.username = username
+        self.password = self._encrypt_pw(password)
+        self.is_logged_in = False
+
+    def _encrypt_pw(self, password):
+        '''Encrypt the password with the username and return
+        the sha digest.'''
+        hash_string = (self.username + password)
+        hash_string = hash_string.encode("utf8")
+        return hashlib.sha256(hash_string).hexdigest()
+
+    def check_password(self, password):
+        '''Return True if the password is valid for this
+        user, false otherwise.'''
+        encrypted = self._encrypt_pw(password)
+        return encrypted == self.password
+
+
+class AuthException(Exception):
+    def __init__(self, username, user=None):
+        super().__init__(username)
+        self.username = username
+        self.user = user
+
+
+class UsernameAlreadyExists(AuthException):
+    pass
+
+
+class PasswordTooShort(AuthException):
+    pass
