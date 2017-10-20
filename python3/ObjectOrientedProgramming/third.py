5cebea56df8cc2305d1f07cd25cb19ece6d53c4f
Asher Guan <>
2017-10-20 14:53:47 +0800

    ducktype - flac

--- python3/ObjectOrientedProgramming/third.py
+++ python3/ObjectOrientedProgramming/third.py
@@ -259,3 +259,15 @@ class MediaLoader(metaclass=abc.ABCMeta):
             if set(cls.__abstractmethods__) <= attrs:
                 return True
         return NotImplemented
+
+
+# ducktype flac
+class FlacFile:
+    def __init__(self, filename):
+        if not filename.endswith(".flac"):
+            raise Exception("Invalid file format")
+
+        self.filename = filename
+
+    def play(self):
+        print("playing {} as flac movie".format(self.filename))
