26f88021c0e463dfd46ffb2e6db70a1c2deaa4dd
Asher Guan <>
2017-10-21 12:41:12 +0800

    python ugly as java

--- python3/ObjectOrientedProgramming/fifth.py
+++ python3/ObjectOrientedProgramming/fifth.py
@@ -57,3 +57,16 @@ class Polygon2:
         for i in range(len(self.vertices)):
             perimeter += points[i].distance(points[i+1])
         return perimeter
+
+
+# python ugly as java
+class Color:
+    def __init__(self, rgb_value, name):
+        self._rgb_value = rgb_value
+        self._name = name
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
