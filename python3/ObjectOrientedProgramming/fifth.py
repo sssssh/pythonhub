0c47acd1e76a3a04147fc749fd0c1dba5aa2a488
Asher Guan <>
2017-10-21 12:47:16 +0800

    method -> setting name

--- python3/ObjectOrientedProgramming/fifth.py
+++ python3/ObjectOrientedProgramming/fifth.py
@@ -82,3 +82,18 @@ class Color2:
 c = Color2("#ff0000", "bright red")
 print(c.name)
 c.name = "red"
+
+
+# setting name in method
+class Color3:
+    def __init__(self, rgb_value, name):
+        self._rgb_value = rgb_value
+        self._name = name
+
+    def set_name(self, name):
+        if not name:
+            raise Exception("Invalid Name")
+        self._name = name
+
+    def get_name(self):
+        return self._name
