4b6d47192d082c66e0ac2b9a27409df4c449c864
Asher Guan <>
2017-10-22 11:15:50 +0800

    property decorator arguments

--- python3/ObjectOrientedProgramming/fifth.py
+++ python3/ObjectOrientedProgramming/fifth.py
@@ -139,3 +139,22 @@ class Foo:
     @property
     def foo(self):
         return "bar"
+
+
+# property decorator arguments
+class Silly2:
+    @property
+    def silly(self):
+        "This is a silly property"
+        print("You are getting silly")
+        return self._silly
+
+    @silly.setter
+    def silly(self, value):
+        print("You are making silly {}".format(value))
+        self._silly = value
+
+    @silly.deleter
+    def silly(self):
+        print("Whoah, you killed silly!")
+        del self._silly
