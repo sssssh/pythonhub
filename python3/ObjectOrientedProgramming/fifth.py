c64a89ad35fea098afd2f9bc8aeeb78c0795b040
Asher Guan <>
2017-10-22 11:09:42 +0800

    silly - property arguments

--- python3/ObjectOrientedProgramming/fifth.py
+++ python3/ObjectOrientedProgramming/fifth.py
@@ -114,3 +114,21 @@ class Color4:
         return self._name
 
     name = property(_get_name, _set_name)
+
+
+# property arguments
+class Silly:
+    def _get_silly(self):
+        print("You are getting silly")
+        return self._silly
+
+    def _set_silly(self, value):
+        print("You are making silly {}".format(value))
+        self._silly = value
+
+    def _del_silly(self):
+        print("Whoah, you killed silly!")
+        del self._silly
+
+    silly = property(_get_silly, _set_silly, _del_silly,
+                     "This is a silly property")
