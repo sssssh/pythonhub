cd397bb9daf1865db5e084ade6262692ed7838c7
Asher Guan <>
2017-10-22 10:47:17 +0800

    setting name property

--- python3/ObjectOrientedProgramming/fifth.py
+++ python3/ObjectOrientedProgramming/fifth.py
@@ -97,3 +97,20 @@ class Color3:
 
     def get_name(self):
         return self._name
+
+
+# setting name property
+class Color4:
+    def __init__(self, rgb_value, name):
+        self.rgb_value = rgb_value
+        self._name = name
+
+    def _set_name(self, name):
+        if not name:
+            raise Exception("Invalid Name")
+        self._name = name
+
+    def _get_name(self):
+        return self._name
+
+    name = property(_get_name, _set_name)
