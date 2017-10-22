6732bf56cb56c0b218b328f0a0889485bc385919
Asher Guan <python.sth@gmail.com>
2017-10-22 17:24:19 +0800

    dict stocks

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -20,3 +20,8 @@ mid_value, date = middle(("GOOG", 613.30, 625.86, 610.50),
 # named tuple
 Stock = namedtuple("Stock", "symbol current high low")
 stock = Stock("FB", 75.00, high=75.03, low=74.90)
+
+
+# dict stocks
+stocks = {"GOOG": (613.30, 625.86, 610.50),
+          "MSFT": (30.25, 30.70, 30.19)}
