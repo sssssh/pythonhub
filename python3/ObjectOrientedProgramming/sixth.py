5bd8fc36d11a2e1266d2571799b8284262a3f1e2
Asher Guan <python.sth@gmail.com>
2017-10-23 00:17:32 +0800

    defaultdict custom function

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -67,3 +67,16 @@ def letter_frequency2(sentence):
     for letter in sentence:
         frequencies[letter] += 1
     return frequencies
+
+
+# defaultdict custom function
+num_items = 0
+
+
+def tuple_counter():
+    global num_items
+    num_items += 1
+    return (num_items, [])
+
+
+d = defaultdict(tuple_counter)
