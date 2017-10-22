6617224b837021069659440eb774b2ee681800c2
Asher Guan <python.sth@gmail.com>
2017-10-22 17:09:33 +0800

    character class

--- python3/ObjectOrientedProgramming/fifth.py
+++ python3/ObjectOrientedProgramming/fifth.py
@@ -403,3 +403,20 @@ class Document2:
 @property
 def string(self):
     return "".join(self.characters)
+
+
+# character class
+class Character:
+    def __init__(self, character,
+                 bold=False, italic=False, underline=False):
+        assert len(character) == 1
+        self.character = character
+        self.bold = self.bold
+        self.italic = italic
+        self.underline = underline
+
+    def __str__(self):
+        bold = "*" if self.bold else ''
+        italic = "/" if self.italic else ''
+        underline = "_" if self.underline else ''
+        return bold + italic + underline + self.character
