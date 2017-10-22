375c139f771c896b61ef8cad4c9c593cb7a76a09
Asher Guan <python.sth@gmail.com>
2017-10-22 17:21:32 +0800

    pass tuple to function

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -1,3 +1,16 @@
+import datetime
+
+
 # empty object
 class MyObject:
     pass
+
+
+# pass tuple to function
+def middle(stock, date):
+    symbol, current, high, low = stock
+    return (((high + low) / 2), date)
+
+
+mid_value, date = middle(("GOOG", 613.30, 625.86, 610.50),
+                         datetime.date(2010, 1, 6))
