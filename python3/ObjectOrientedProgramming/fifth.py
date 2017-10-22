33dcbda86a572affc5151c2a2f66d525cce743d9
Asher Guan <>
2017-10-22 14:00:15 +0800

    zip replace inheritance

--- python3/ObjectOrientedProgramming/fifth.py
+++ python3/ObjectOrientedProgramming/fifth.py
@@ -1,3 +1,4 @@
+import os
 import math
 import sys
 import shutil
@@ -293,3 +294,23 @@ class ZipProcessor:
             for filename in self.temp_directory.iterdir():
                 file.write(str(filename), filename.name)
         shutil.rmtree(str(self.temp_directory))
+
+
+# zip replace inheritance
+class ZipReplace2(ZipProcessor):
+    def __init__(self, filename, search_string,
+                 replace_string):
+        super().__init__(filename)
+        self.search_string = search_string
+        self.replace_string = replace_string
+
+    def process_files(self):
+        '''perform a search and replace on all files in the
+        temporary directory'''
+        for filename in self.temp_directory.iterdir():
+            with filename.open() as file:
+                contents = file.read()
+            contents = contents.replace(
+                    self.search_string, self.replace_string)
+            with filename.open("w") as file:
+                file.write(contents)
