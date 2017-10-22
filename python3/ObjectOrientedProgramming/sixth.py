dc6bbb6cc471624cc8e6d9ec83127cca6f040f61
Asher Guan <python.sth@gmail.com>
2017-10-23 00:07:56 +0800

    random key dict

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -25,3 +25,28 @@ stock = Stock("FB", 75.00, high=75.03, low=74.90)
 # dict stocks
 stocks = {"GOOG": (613.30, 625.86, 610.50),
           "MSFT": (30.25, 30.70, 30.19)}
+
+
+# random key dict
+random_keys = {}
+random_keys["astring"] = "somestring"
+random_keys[5] = "aninteger"
+random_keys[25.2] = "floats work too"
+random_keys[("abc", 123)] = "so do tuples"
+
+
+class AnObject:
+    def __init__(self, avalue):
+        self.avalue = avalue
+
+
+my_object = AnObject(14)
+random_keys[my_object] = "We can even store objects"
+my_object.avalue = 12
+try:
+    random_keys[[1, 2, 3]] = "we can't store lists though"  # key (list) -> unhashable type: 'list'
+except:
+    print("unable to store list\n")
+
+for key, value in random_keys.items():
+    print("{} has value {}".format(key, value))
