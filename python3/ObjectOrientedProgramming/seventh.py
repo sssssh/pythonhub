c866977373071e7fc6ab8f4d6c3a2c9157aedff3
Asher Guan <python.sth@gmail.com>
2017-10-23 17:05:54 +0800

    send mailing

--- python3/ObjectOrientedProgramming/seventh.py
+++ python3/ObjectOrientedProgramming/seventh.py
@@ -343,3 +343,11 @@ def emails_in_groups(self, *groups):
         if g & groups:
             emails.add(e)
     return emails
+
+
+# send emails
+def send_mailing(self, subject, message, from_addr,
+                 *groups, headers=None):
+        emails = self.emails_in_groups(*groups)
+        send_email(subject, message, from_addr,
+                   *emails, headers=headers)
