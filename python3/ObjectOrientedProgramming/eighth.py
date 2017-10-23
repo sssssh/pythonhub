816c25d87d0195dbcd04c5b385348fa53ee2b7b3
Asher Guan <python.sth@gmail.com>
2017-10-23 17:44:53 +0800

    tabular

--- python3/ObjectOrientedProgramming/eighth.py
+++ python3/ObjectOrientedProgramming/eighth.py
@@ -119,3 +119,16 @@ print("Sub: ${0} Tax: ${1} Total: ${total}".format(
 print("Sub: ${0:0.2f} Tax: ${1:0.2f} "
       "Total: ${total:0.2f}".format(
           subtotal, tax, total=total))
+
+
+# tabular
+orders = [('burger', 2, 5),
+          ('fries', 3.5, 1),
+          ('cola', 1.75, 3)]
+
+
+print("PRODUCT    QUANTITY    PRICE    SUBTOTAL")
+for product, price, quantity in orders:
+    subtotal = price * quantity
+    print("{0:10s}{1: ^9d}    ${2: <8.2f}${3: >7.2f}".format(
+        product, quantity, price, subtotal))
