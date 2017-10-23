b475aa35b5239a132630696c84d92f5c9883cbbc
Asher Guan <python.sth@gmail.com>
2017-10-23 15:43:08 +0800

    kwarg options

--- python3/ObjectOrientedProgramming/seventh.py
+++ python3/ObjectOrientedProgramming/seventh.py
@@ -119,3 +119,21 @@ def get_pages2(*links):
     for link in links:
         # download the link with urllib
         print(link)
+
+
+# kwarg options
+class Options:
+    default_options = {
+        "port": 21,
+        "host": 'localhost',
+        "username": None,
+        "password": None,
+        "debug": False,
+        }
+
+    def __init__(self, **kwargs):
+        self.options = dict(Options.default_options)
+        self.options.update(kwargs)
+
+    def __getitem__(self, key):
+        return self.options[key]
