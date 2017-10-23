30463ae37d94ad47444d7ea07080ebb167fa32a8
Asher Guan <python.sth@gmail.com>
2017-10-23 14:42:06 +0800

    normalize url

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -260,3 +260,14 @@ class LinkCollector:
         page = str(urlopen(full_url).read())
         links = LINK_REGEX.findall(page)
         return links
+
+
+# normalize urllib
+def normalize_url(self, path, link):
+    if link.startswith("http://"):
+        return link
+    elif link.startswith("/"):
+        return self.url + link
+    else:
+        return self.url + path.rpartition('/'
+                                          )[0] + '/' + link
