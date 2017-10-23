128f68a375d737c96e829523ce9df47d2eec65d2
Asher Guan <python.sth@gmail.com>
2017-10-23 16:01:05 +0800

    timer

--- python3/ObjectOrientedProgramming/seventh.py
+++ python3/ObjectOrientedProgramming/seventh.py
@@ -1,3 +1,5 @@
+import time
+import datetime
 import shutil
 import os.path
 import string
@@ -198,3 +200,32 @@ def another_function(function):
 
 another_function(my_function)
 another_function(second_function)
+
+
+# timer
+class TimedEvent:
+    def __init__(self, endtime, callback):
+        self.endtime = endtime
+        self.callback = callback
+
+    def ready(self):
+        return self.endtime <= datetime.datetime.now()
+
+
+class Timer:
+    def __init__(self):
+        self.events = []
+
+    def call_after(self, delay, callback):
+        end_time = datetime.datetime.now() + \
+                   datetime.timedelta(seconds=delay)
+
+        self.events.append(TimedEvent(end_time, callback))
+
+    def run(self):
+        while True:
+            ready_events = (e for e in self.events if e.ready())
+            for event in ready_events:
+                event.callback(self)
+                self.events.remove(event)
+            time.sleep(0.5)
