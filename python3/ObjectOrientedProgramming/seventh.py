4b38eec08df870fa6d6484c6c49c727fff6f82b7
Asher Guan <python.sth@gmail.com>
2017-10-23 15:54:55 +0800

    function object -> func.description

--- python3/ObjectOrientedProgramming/seventh.py
+++ python3/ObjectOrientedProgramming/seventh.py
@@ -168,3 +168,33 @@ def augmented_move(target_folder, *filenames,
 # unpacking arguments
 def show_args(arg1, arg2, arg3="THREE"):
     print(arg1, arg2, arg3)
+
+
+# function objects
+def my_function():
+    print("The Function Was Called")
+
+
+my_function.description = "A silly function"
+
+
+def second_function():
+    print("The second was called")
+
+
+second_function.description = "A sillier function."
+
+
+def another_function(function):
+    print("The description:", end=" ")
+    print(function.description)
+    print("The name:", end=" ")
+    print(function.__name__)
+    print("The class:", end=" ")
+    print(function.__class__)
+    print("Now I'll call the function passed in")
+    function()
+
+
+another_function(my_function)
+another_function(second_function)
