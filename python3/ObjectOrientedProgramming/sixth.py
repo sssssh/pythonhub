4abf07d71748ba56a34c6afeb37ec5861013506f
Asher Guan <python.sth@gmail.com>
2017-10-23 11:06:23 +0800

    song artist set

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -140,3 +140,18 @@ class WeirdSortee:
             self.number == object.number,
             self.sort_num == object.number
         ))
+
+
+# song artist set
+song_library = [("Phantom Of The Opera", "Sarah Brightman"),
+        ("Knocking On Heaven's Door", "Guns N' Roses"),
+        ("Captain Nemo", "Sarah Brightman"),
+        ("Patterns In The Ivy", "Opeth"),
+        ("November Rain", "Guns N' Roses"),
+        ("Beautiful", "Sarah Brightman"),
+        ("Mal's Song", "Vixy and Tony")]
+
+
+artists = set()
+for song, artist in song_library:
+    artists.add(artist)
