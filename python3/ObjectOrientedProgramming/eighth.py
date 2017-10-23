b10615d4265787f0182a0da13624e77a4200a786
Asher Guan <python.sth@gmail.com>
2017-10-23 17:40:43 +0800

    no format

--- python3/ObjectOrientedProgramming/eighth.py
+++ python3/ObjectOrientedProgramming/eighth.py
@@ -103,3 +103,13 @@ Subject: {0.subject}
 
 {0.message}"""
 print(template.format(email2))
+
+
+# no format
+subtotal = 12.32
+tax = subtotal * 0.07
+total = subtotal + tax
+
+
+print("Sub: ${0} Tax: ${1} Total: ${total}".format(
+    subtotal, tax, total=total))
