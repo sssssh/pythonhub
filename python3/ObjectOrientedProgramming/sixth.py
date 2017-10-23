26797ecb96b3860234ced91fb7b956fb98eb5bed
Asher Guan <python.sth@gmail.com>
2017-10-23 14:36:17 +0800

    dict sorted

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -1,6 +1,7 @@
 import string
 import datetime
 from collections import namedtuple, defaultdict, Counter
+from collections import KeysView, ItemsView, ValuesView
 from functools import total_ordering
 
 
@@ -209,3 +210,35 @@ while it.has_next():
 class SillyInt(int):
     def __add__(self, num):
         return 0
+
+
+# dict sirted
+class DictSorted(dict):
+    def __new__(*args, **kwargs):
+        new_dict = dict.__new__(*args, **kwargs)
+        new_dict.ordered_keys = []
+        return new_dict
+
+    def __setitem__(self, key, value):
+        '''self[key] = value syntax'''
+        if key not in self.ordered_keys:
+            self.ordered_keys.append(key)
+        super().__setitem__(key, value)
+
+    def setdefault(self, key, value):
+        if key not in self.ordered_keys:
+            self.ordered_keys.append(key)
+        return super().setdefault(key, value)
+
+    def keys(self):
+        return KeysView(self)
+
+    def values(self):
+        return ValuesView(self)
+
+    def items(self):
+        return ItemsView(self)
+
+    def __iter__(self):
+        '''for x in self syntax'''
+        return self.ordered_keys.__init__()
