572f6f7e6905c1b513de20f386982aaf2b31854d
Asher Guan <python.sth@gmail.com>
2017-10-23 14:52:09 +0800

    visted links sets

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -271,3 +271,36 @@ def normalize_url(self, path, link):
     else:
         return self.url + path.rpartition('/'
                                           )[0] + '/' + link
+
+
+# visited links sets
+class LinkCollector:
+    def __init__(self, url):
+        # urlparse('http://www.baidu.com/index.php?username=guol')
+        # ParseResult(scheme='http', netloc='www.baidu.com', path='/index.php', params='', query='username=guol', fragment='')
+        # url.netloc -> www.baidu.com
+        self.url = "http://" + urlparse(url).netloc
+        self.collected_links = set()
+        self.visited_links = set()
+
+    def collect_links(self, path="/"):
+        full_url = self.url + path
+        self.visited_links.add(full_url)
+        page = str(urlopen(full_url).read())
+        links = LINK_REGEX.findall(page)
+        links = {self.normalize_url(path, link
+                                    ) for link in links}
+        self.collected_links = links.union(
+            self.collected_links)
+        unvisited_links = links.difference(
+            self.visited_links)
+        print(links, self.visited_links,
+              self.collected_links, unvisited_links)
+
+    def normalize_url(self, path, link):
+        if link.startswith("http://"):
+            return link
+        elif link.startswith("/"):
+            return self.url + link
+        else:
+            return self.url + path.rpartition('/')[0] + '/' + link
