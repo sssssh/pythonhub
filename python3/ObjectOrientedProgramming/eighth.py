b79ac4654787ed9a84938823b6ba063cfdb94589
Asher Guan <python.sth@gmail.com>
2017-10-23 17:53:00 +0800

    decode unicode

--- python3/ObjectOrientedProgramming/eighth.py
+++ python3/ObjectOrientedProgramming/eighth.py
@@ -146,3 +146,11 @@ print("{0:%Y-%m-%d %I:%M%p }".format(
 characters = b'\x63\x6c\x69\x63\x68\xe9'
 print(characters)
 print(characters.decode("latin-1"))
+
+
+# decode unicode
+characters = "cliché"
+print(characters.encode("UTF-8"))
+print(characters.encode("latin-1"))
+print(characters.encode("CP437"))
+print(characters.encode("ascii"))
