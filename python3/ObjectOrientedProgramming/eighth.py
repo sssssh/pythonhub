c86724183302b7c118951cd27dd752f077ecc1b6
Asher Guan <python.sth@gmail.com>
2017-10-23 17:24:48 +0800

    brace escape

--- python3/ObjectOrientedProgramming/eighth.py
+++ python3/ObjectOrientedProgramming/eighth.py
@@ -22,3 +22,14 @@ print(template2.format('Dusty', 'writing'))
 # format some positions broken
 # template3 = "Hello {}, you are {}. Your name is {0}."
 # print(template3.format('Dusty', 'writing'))  # crash
+
+
+# brace escape
+template4 = """
+public class {0} {{
+    public static void main(String[] args) {{
+        System.out.println("{1}");
+    }}
+}}"""
+
+print(template4.format("MyClass", "print('hello world')"))
