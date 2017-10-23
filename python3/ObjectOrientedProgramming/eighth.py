ad41f67a4d4fe4003561a3e9b242c4b082e3ee34
Asher Guan <python.sth@gmail.com>
2017-10-23 18:05:16 +0800

    json objects

--- python3/ObjectOrientedProgramming/eighth.py
+++ python3/ObjectOrientedProgramming/eighth.py
@@ -1,3 +1,4 @@
+import json
 import datetime
 import pickle
 import re
@@ -250,3 +251,31 @@ class UpdatedURL:
     def __setstate__(self, data):
         self.__dict__ = data
         self.schedule()
+
+
+# json objects
+class Contact:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def full_name(self):
+        return ("{} {}".format(self.first, self.last))
+
+
+class ContactEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Contact):
+            return {'is_contact': True,
+                    'first': obj.first,
+                    'last': obj.last,
+                    'full': obj.full_name}
+        return super().default(obj)
+
+
+def decode_contact(dic):
+    if dic.get('is_contact'):
+        return Contact(dic['first'], dic['last'])
+    else:
+        return dic
