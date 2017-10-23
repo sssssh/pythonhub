20a78171df9fea349dc4a03d8073c29c296b8ad4
Asher Guan <python.sth@gmail.com>
2017-10-23 11:11:55 +0800

    set operations

--- python3/ObjectOrientedProgramming/sixth.py
+++ python3/ObjectOrientedProgramming/sixth.py
@@ -168,3 +168,18 @@ print("All: {}".format(my_artists.union(auburns_artists)))
 print("Both: {}".format(auburns_artists.intersection(my_artists)))
 print("Either but not both: {}".format(
     my_artists.symmetric_difference(auburns_artists)))
+
+
+# set operations
+bands = {"Guns N' Roses", "Opeth"}
+
+
+print("my_artists is to bands:")
+print("issuperset: {}".format(my_artists.issuperset(bands)))
+print("issubset: {}".format(my_artists.issubset(bands)))
+print("difference: {}".format(my_artists.difference(bands)))
+print("*" * 20)
+print("bands is to my_artists:")
+print("issuperset: {}".format(bands.issuperset(my_artists)))
+print("issubset: {}".format(bands.issubset(my_artists)))
+print("difference: {}".format(bands.difference(my_artists)))
