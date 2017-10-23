48057d11064515e78b9e64edccb55512fbb1eaf5
Asher Guan <python.sth@gmail.com>
2017-10-23 16:23:59 +0800

    callable repeat

--- python3/ObjectOrientedProgramming/seventh.py
+++ python3/ObjectOrientedProgramming/seventh.py
@@ -286,3 +286,20 @@ a = A()
 a.print()
 a.print = fake_print
 a.print()
+
+
+# callable repeat
+class Repeater2:
+    def __init__(self):
+        self.count = 0
+
+    def __call__(self, timer):
+        format_time("{now}: repeat {0}", self.count)
+        self.count += 1
+        timer.call_after(5, self)
+
+
+timer = Timer()
+timer.call_after(5, Repeater())
+format_time("{now}: Starting")
+timer.run()
