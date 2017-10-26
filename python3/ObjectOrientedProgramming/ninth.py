3aa3e87a75f710e9938cb7a8025021045369d2f1
Asher Guan <python.sth@gmail.com>
2017-10-26 10:44:56 +0800

    listcomp converter

--- python3/ObjectOrientedProgramming/ninth.py
+++ python3/ObjectOrientedProgramming/ninth.py
@@ -29,3 +29,7 @@ input_strings = ['1', '5', '28', '131', '3']
 output_integers = []
 for num in input_strings:
     output_integers.append(int(num))
+
+
+# listcomp converter
+listcomp_integers = [int(num) for num in input_strings]
