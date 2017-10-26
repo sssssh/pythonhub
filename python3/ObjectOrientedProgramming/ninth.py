5e54b75c079d81af48b249ffafc75e74db269e75
Asher Guan <python.sth@gmail.com>
2017-10-26 11:03:23 +0800

    log delete warning object

--- python3/ObjectOrientedProgramming/ninth.py
+++ python3/ObjectOrientedProgramming/ninth.py
@@ -89,3 +89,27 @@ with open(existfilename) as infile:
         for l in infile:
             if 'WARNING' in l:
                 outfile.write(l.replace('\tWARNING', ''))
+
+
+# delete warnings
+class WarningFilter:
+    def __init__(self, insequence):
+        self.insequence = insequence
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        l = self.insequence.readline()
+        while l and 'WARNING' not in l:
+            l = self.insequence.readline()
+        if not l:
+            raise StopIteration
+        return l.replace('\WARNING', '')
+
+
+with open(existfilename) as infile:
+    with open(newfilename, "w") as outfile:
+        filter = WarningFilter(infile)
+        for l in filter:
+            outfile.write(l)
