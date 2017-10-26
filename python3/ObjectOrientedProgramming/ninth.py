9e3415067120b174ee668c95d9854c2cd57f530a
Asher Guan <python.sth@gmail.com>
2017-10-26 11:28:37 +0800

    kernel log

--- python3/ObjectOrientedProgramming/ninth.py
+++ python3/ObjectOrientedProgramming/ninth.py
@@ -1,3 +1,4 @@
+import re
 from collections import namedtuple
 
 
@@ -192,3 +193,28 @@ def tally():
     while True:
         increment = yield score
         score += 1
+
+
+# kernel log
+def match_regex(filename, regex):
+    with open(filename) as file:
+        lines = file.readlines()
+    for line in reversed(lines):
+        match = re.match(regex, line)
+        if match:
+            regex = yield match.groups()[0]
+
+
+def get_serials(filename):
+    ERROR_RE = 'XFS ERROR (\[sd[a-z]\])'
+    matcher = match_regex(filename, ERROR_RE)
+    device = next(matcher)
+    while True:
+        bus = matcher.send('(sd \S+) {}.*'.format(re.escape(device)))
+        serial = matcher.send('{} \(SERIAL=([^)]*)\)'.format(bus))
+        yield serial
+        device = matcher.send(ERROR_RE)
+
+
+for serial_number in get_serials('EXAMPLE_LOG.log'):
+    print(serial_number)
