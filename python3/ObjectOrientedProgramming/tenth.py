26c00035103cd07ad3d62707578975052dc01d1a
Asher Guan <python.sth@gmail.com>
2017-10-27 11:47:18 +0800

    simple socket

---
+++ python3/ObjectOrientedProgramming/tenth.py
@@ -0,0 +1,19 @@
+import socket
+
+
+# simple socket
+def respond(client):
+    res = input("Enter a value: ")
+    client.send(bytes(res, 'utf-8'))
+    client.close()
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 2401))
+server.listen(1)
+try:
+    while True:
+        cilent, addr = server.accept()
+        respond(client)
+finally:
+    server.close()
