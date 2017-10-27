10959e3158ee906e53cbba92cdf419e8448dd0f1
Asher Guan <python.sth@gmail.com>
2017-10-27 14:43:40 +0800

    gzip decorator

--- python3/ObjectOrientedProgramming/tenth.py
+++ python3/ObjectOrientedProgramming/tenth.py
@@ -1,4 +1,6 @@
 import socket
+import gzip
+from io import BytesIO
 
 
 # simple socket
@@ -44,5 +46,32 @@ try:
     while True:
         client, addr = server.accept()
         respond(LogSocket(client))
+finally:
+    server.close()
+
+
+# gzip decorator
+class GzipSocket:
+    def __init__(self, socket):
+        self.socket = socket
+
+    def send(self, data):
+        buf = BytesIO()
+        zipfile = gzip.GzipFile(fileobj=buf, mode="w")
+        zipfile.write(data)
+        zipfile.close()
+        self.socket.send(buf.getvalue())
+
+    def close(self):
+        self.socket.close()
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('', 2401))
+server.listen(1)
+try:
+    while True:
+        client, addr = server.accept()
+        respond(GzipSocket(client))
 finally:
     server.close()
