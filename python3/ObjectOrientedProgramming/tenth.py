d2c33e3212966772e4927344cd3abe1d4e87b8c3
Asher Guan <python.sth@gmail.com>
2017-10-27 14:52:21 +0800

    log decorator

--- python3/ObjectOrientedProgramming/tenth.py
+++ python3/ObjectOrientedProgramming/tenth.py
@@ -1,3 +1,4 @@
+import time
 import socket
 import gzip
 from io import BytesIO
@@ -75,3 +76,29 @@ try:
         respond(GzipSocket(client))
 finally:
     server.close()
+
+
+# logging decorator
+def log_calls(func):
+    def wrapper(*args, **kwargs):
+        now = time.time()
+        print("Calling {0} with {1} and {2}".format(
+            func.__name__, args, kwargs))
+        return_value = func(*args, **kwargs)
+        print("Executed {0} in {1}ms".format(
+            func.__name__, time.time() - now))
+        return return_value
+    return wrapper
+
+
+def test1(a, b, c):
+    print("\ttest1 called")
+
+
+def test2(a, b):
+    print("\ttest2 called")
+
+
+def test3(a, b):
+    print("\ttest3 called")
+    time.sleep(1)
