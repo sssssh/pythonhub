3d0863bbce93183266276fc37c865b997bc27228
Asher Guan <python.sth@gmail.com>
2017-10-27 14:39:26 +0800

    logging decorator

--- python3/ObjectOrientedProgramming/tenth.py
+++ python3/ObjectOrientedProgramming/tenth.py
@@ -24,3 +24,25 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 2401))
 print("Received: {}".format(client.recv(1024)))
 client.close()
+
+
+# logging decorator
+class LogSocket:
+    def __init__(self, socket):
+        self.socket = socket
+
+    def send(self, data):
+        print("Sending {0} to {1}".format(
+            data, self.socket.getpeername()[0]))
+        self.socket.send(data)
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('', 2401))
+server.listen(1)
+try:
+    while True:
+        client, addr = server.accept()
+        respond(LogSocket(client))
+finally:
+    server.close()
