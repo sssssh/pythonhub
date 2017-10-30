4cb92cd0270bd8e594e2711d9ff7c26d5550d290
Asher Guan <python.sth@gmail.com>
2017-10-30 10:36:07 +0800

    how to execute event loop - callback or future

--- python3/HighPerformancePython/_Concurrency/intro_asynchronous_programming.py
+++ python3/HighPerformancePython/_Concurrency/intro_asynchronous_programming.py
@@ -24,11 +24,40 @@ def do_world():
     eventloop.put(do_hello)
 
 
+# 事件循环编程能采取两种方式：回调或者future
+def save_value(value, callback):
+    print("Saving {} to database".format(value))
+    save_result_to_db(value, callback)
+
+
+def print_response(db_response):
+    print("Response from database: {}".format(db_response))
+
+
+def save_result_to_db(result, callback):
+    pass
+
+
+# futures
+@coroutine
+def save_value(value, callback):
+    print("Saving {} to database".format(value))
+    db_response = yield save_result_to_db(value, callback)
+    print("Response from database: {}".format(db_response))
+
+
 if __name__ == "__main__":
     eventloop = EventLoop()
     eventloop.put(do_hello)
     eventloop.start()
 
+    # callback
+    eventloop.put(
+        partial(save_value, "Hello World", print_response)
+
+    # futures
+    eventloop.put(
+        partial(save_value, "Hello World")
 
 # 事件循环 和 异步
 # 函数之间的切换会有开销。内核必须花费时间来设置在内存中被调用的函数，缓存的状态将变得不可预测。
