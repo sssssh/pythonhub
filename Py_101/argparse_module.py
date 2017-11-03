9d298d88d5b1d41f139e3cf8b5d85e4e82b39a5d
Asher Guan <python.sth@gmail.com>
2017-11-03 10:31:56 +0800

    added arguments

--- Py_101/argparse_module.py
+++ Py_101/argparse_module.py
@@ -8,7 +8,13 @@ def get_args():
         description="A simple argument parser",
         epilog="This is where you might put example usage"
     )
-    return parser.parse_args()
+    # required argument
+    parser.add_argument('-x', action="store", required=True,
+                        help='Help text for option X')
+    # optional arguments
+    parser.add_argument('-y', help='Help text for option Y', default=False)
+    parser.add_argument('-z', help='Help text for option Z', type=int)
+    print( parser.parse_args())
 
 
 if __name__ == '__main__':
