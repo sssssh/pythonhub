0c6fcc21c23d477d0eed1e09691cfcce7913938b
Asher Guan <python.sth@gmail.com>
2017-11-03 10:33:11 +0800

    added Long Options

--- Py_101/argparse_module.py
+++ Py_101/argparse_module.py
@@ -9,12 +9,12 @@ def get_args():
         epilog="This is where you might put example usage"
     )
     # required argument
-    parser.add_argument('-x', action="store", required=True,
+    parser.add_argument('-x', '--execute', action="store", required=True,
                         help='Help text for option X')
     # optional arguments
     parser.add_argument('-y', help='Help text for option Y', default=False)
     parser.add_argument('-z', help='Help text for option Z', type=int)
-    print( parser.parse_args())
+    print(parser.parse_args())
 
 
 if __name__ == '__main__':
