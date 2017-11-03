c2b4275a05a60307aa976ade03bf3a896141126b
Asher Guan <python.sth@gmail.com>
2017-11-03 11:30:15 +0800

    how to use defaultdict

--- Py_101/defaultdict_module.py
+++ Py_101/defaultdict_module.py
@@ -20,3 +20,35 @@ for word in words:
     default_dict[word] += 1
 
 print(default_dict)
+
+
+# _list
+my_dict = {}
+my_list = [(1234, 100.23), (345, 10.45), (1234, 75.00),
+           (345, 222.66), (678, 300.25), (1234, 35.67)]
+
+for acct_num, value in my_list:
+    if acct_num in my_dict:
+        my_dict[acct_num].append(value)
+    else:
+        my_dict[acct_num] = [value]
+
+
+d = defaultdict(list)
+for acct_num, value in my_list:
+    d[acct_num].append(value)
+
+
+print(d)
+
+
+# lambda
+animal = defaultdict(lambda: "Monkey")
+animal['Sam'] = 'Tiger'
+print(animal['Nick'])
+print(animal)
+
+
+# KeyError
+x = defaultdict(None)
+print(x['Mike'])
