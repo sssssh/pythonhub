098e8959dec1dc15ac8c46ddb06a421fb9e2cc75
Asher Guan <python.sth@gmail.com>
2017-11-03 10:25:35 +0800

    simple argument parser - parser.print_help()

---
+++ Py_101/argparse_module.py
@@ -0,0 +1,11 @@
+# An Intro to Argument Parsing using argparse
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    description="A simple argument parser",
+    epilog="This is where you might put example usage"
+    )
+
+
+parser.print_help()
