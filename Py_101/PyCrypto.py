9a68efd7140a9d3e811a738b7d05d356242d94bf
Asher Guan <python.sth@gmail.com>
2017-11-04 13:17:37 +0800

    DES, RSA

---
+++ Py_101/PyCrypto.py
@@ -0,0 +1,35 @@
+# pip install pycryptodome
+# pip install pycryptodomex
+from Crypto.Cipher import DES, RSA
+
+
+# encrypting a string
+key = b'abcdefgh'
+
+
+def pad(text):
+    while len(text) % 8 != 0:
+        text += b' '
+    return text
+
+
+des = DES.new(key, DES.MODE_ECB)
+text = b'Python rocks!'
+padded_text = pad(text)
+encrypted_text = des.encrypt(text)
+encrypted_text = des.encrypt(padded_text)
+
+
+# create an RSA key
+code = 'nooneknows'
+key = RSA.generate(2048)
+encrypted_key = key.exportKey(passphrase=code, pkcs=8,
+                              protection="scryptAndAES128-CBC")
+
+
+with open('/path_to_private_key/my_private_rsa_key.bin', 'wb') as f:
+    f.write(encrypted_key)
+
+
+with open('/path_to_public_key/my_rsa_public.pem', 'wb') as f:
+    f.write(key.publickey().exportKey())
