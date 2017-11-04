26fcd1bf7caf7dc7052dfce6de3268bf14d8ee3c
Asher Guan <python.sth@gmail.com>
2017-11-04 10:33:59 +0800

    module import checking

--- Py_101/importlib_module.py
+++ Py_101/importlib_module.py
@@ -1,13 +1,33 @@
-import importlib
+import importlib.util
 
 
-def dynamic_import(module):
-    return importlib.import_module(module)
+def check_module(module_name):
+    """
+    Checks if module can be imported without actually
+    importing it
+    """
+    module_spec = importlib.util.find_spec(module_name)
+    if module_spec is None:
+        print('Module: {} not found'.format(module_name))
+        return None
+    else:
+        print('Module: {} can be imported'.format(module_name))
+        return module_spec
+
+
+def import_module_from_spec(module_spec):
+    """
+    import the module via the passed in module specification
+    Returns the newly imported module
+    """
+    module = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(module)
+    return module
 
 
 if __name__ == '__main__':
-    module = dynamic_import('foo')
-    module.main()
-
-    module_two = dynamic_import('bar')
-    module_two.main()
+    module_spec = check_module('fake_module')
+    module_spec = check_module('collections')
+    if module_spec:
+        module = import_module_from_spec(module_spec)
+        print(dir(module))
