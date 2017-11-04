ba2344cea2d6fa2c44bd25c7d115ea4e711d63b0
Asher Guan <python.sth@gmail.com>
2017-11-04 23:00:07 +0800

    method resolution order

--- Py_101/super_built-in.py
+++ Py_101/super_built-in.py
@@ -33,3 +33,69 @@ class MyParentClass():
 class SubClass(MyParentClass):
     def __init__(self, x, y):
         super().__init__(x, y)
+
+
+# method resolution order
+class X:
+    def __init__(self):
+        print('X')
+        super().__init__()
+
+
+class Y:
+    def __init__(self):
+        print('Y')
+        super().__init__()
+
+
+class Z(X, Y):
+    pass
+
+
+z = Z()
+print(Z.__mro__)
+
+
+class Base:
+    var = 5
+
+    def __init__(self):
+        pass
+
+
+class X(Base):
+    def __init__(self):
+        print('X')
+        super().__init__()
+
+
+class Y(Base):
+    var = 10
+
+    def __init__(self):
+        print('Y')
+        super().__init__()
+
+
+class Z(X, Y):
+    pass
+
+
+z = Z()
+print(Z.__mro__)
+print(super(Z, z).var)
+
+
+class Base():
+    def __init__(self):
+        s = super()
+        print(s.__thisclass__)
+        print(s.__self_class__)
+        s.__init__()
+
+
+class SubClass(Base):
+    pass
+
+
+sub = SubClass()
