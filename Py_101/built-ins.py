009415b702649d224259e55d2f0ace42c33e4908
Asher Guan <python.sth@gmail.com>
2017-11-04 12:22:15 +0800

    filter

--- Py_101/built-ins.py
+++ Py_101/built-ins.py
@@ -17,3 +17,13 @@ for pos, letter in enumerate(my_string):
 var = 10
 source = 'var * 2'
 print(eval(source))
+
+
+# filter
+def less_than_ten(x):
+    return x < 10
+
+
+my_list = [1, 2, 3, 10, 11, 12]
+for item in filter(less_than_ten, my_list):
+    print(item)
