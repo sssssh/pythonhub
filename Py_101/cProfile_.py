44e620c247a265284254adf1328f66f4a6d5630c
Asher Guan <python.sth@gmail.com>
2017-11-04 13:00:09 +0800

    others profile

--- Py_101/cProfile_.py
+++ Py_101/cProfile_.py
@@ -1,4 +1,15 @@
+import time
 import cProfile
+import line_profile
+import memory_profiler
+import profilehooks
 
 
 print(cProfile.run("[x for x in range(1500)]"))
+
+
+# line_profile
+
+# memory_profiler
+
+# profilehooks
