6acd7c5a4a66c29692736f193edf27f3e3baae6f
Asher Guan <python.sth@gmail.com>
2017-11-05 10:37:08 +0800

    lock.acquire()

--- Py_101/locks_synchronization.py
+++ Py_101/locks_synchronization.py
@@ -2,6 +2,10 @@ import threading
 
 
 total = 0
+lock = threading.Lock()
+
+
+print('------------', total)
 
 
 def update_total(amount):
@@ -10,8 +14,42 @@ def update_total(amount):
     print(total)
 
 
+def update_total2(amount):
+    global total
+    lock.acquire()
+    try:
+        total += amount
+    finally:
+        lock.release()  # ensure htat the lock is always released
+#    except:
+#        pass
+    print(total)
+
+
+def update_total3(amount):
+    global total
+    with lock:
+        total += amount
+    print(total)
+
+
 if __name__ == '__main__':
     for i in range(10):
         my_thread = threading.Thread(
             target=update_total, args=(5,))
         my_thread.start()
+    print('------------', total)
+
+    for i in range(10):
+        my_thread2 = threading.Thread(
+            target=update_total2, args=(6,))
+        my_thread2.start()
+
+    print('------------', total)
+
+    for i in range(10):
+        my_thread3 = threading.Thread(
+            target=update_total3, args=(7,))
+        my_thread3.start()
+
+    print('-----------', total)
