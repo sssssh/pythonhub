7a8240c7bc8b5746f0204e537a2415009ebfe342
Asher Guan <python.sth@gmail.com>
2017-11-06 10:15:39 +0800

    weakref module

--- python3/ObjectOrientedProgramming/eleven.py
+++ python3/ObjectOrientedProgramming/eleven.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 # age calculator
 class AgeCalculator:
     def __init__(self, birthday):
@@ -25,3 +28,25 @@ class DateAgeAdapter:
     def get_age(self, date):
         date = self._str_date(date)
         return self.calculator.calculate_age(date)
+
+
+# age calculator adapt date
+class AgeableDate(datetime.date):
+    def split(self, char):
+        return self.year, self.month, self.day
+
+
+# flyweight factory
+import weakref
+
+
+class CarModel:
+    _models = weakref.WeakValueDictionary()
+
+    def __new__(cls, model_name, *args, **kwargs):
+        model = cls._models.get(model_name)
+        if not model:
+            model = super().__new__(cls)
+            cls._models[model_name] = model
+
+        return model
