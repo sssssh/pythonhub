cafe4999101d48445898c4504a321875a0686b60
Asher Guan <python.sth@gmail.com>
2017-11-07 10:35:35 +0800

    lists vs. a mapping

--- python3/Master_Object_Oriented/Creating_Containers_and_Collectibles/list_vs_set.py
+++ python3/Master_Object_Oriented/Creating_Containers_and_Collectibles/list_vs_set.py
@@ -1,3 +1,7 @@
 import timeit
 timeit.timeit('l.remove(10); l.append(10)', 'l = list(range(20))')
 timeit.timeit('l.remove(10); l.add(10)', 'l = set(range(20))')
+
+# using two parallet lists vs. a mapping
+timeit.timeit('i= k.index(10); v[i]= 0', 'k=list(range(20)); v=list(range(20))')
+timeit.timeit('m[10]= 0', 'm=dict(zip(list(range(20)),list(range(20))))')
