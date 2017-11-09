8275b8c2ac2b72cf4e7a032b909553b74eded602
Asher Guan <python.sth@gmail.com>
2017-11-09 17:58:29 +0800

    the advantage

--- python3/Master_Object_Oriented/Attribute_Access_Properties_and_Descriptors/propery_decorator.py
+++ python3/Master_Object_Oriented/Attribute_Access_Properties_and_Descriptors/propery_decorator.py
@@ -47,9 +47,58 @@ class Hand_Lazy(Hand):
         self._cards.pop(-1)
 
 
+class Hand_Eager(Hand):
+    def __init__(self, dealer_card, *cards):
+        self.dealer_card = dealer_card
+        self.total = 0
+        self._delta_soft = 0
+        self._cards = list()
+        for c in cards:
+            self.card = c
+
+    @property
+    def card(self):
+        return self._cards
+
+    @card.setter
+    def card(self, aCard):
+        self._cards.append(aCard)
+        self._delta_soft = max(aCard.soft-aCard.hard, self._delta_soft)
+        self._hard_total += aCard.hard
+        self._set_total()
+
+    @card.deleter
+    def card(self):
+        removed = self._cards.pop(-1)
+        self._hard_total -= removed.hard
+        self._delta_soft = max(c.soft-c.hard for c in self._cards)
+        self._set_total()
+
+    def _set_total(self):
+        if self._hard_total + self._delta_soft <= 21:
+            self.total = self._hard_total + self._delta_soft
+        else:
+            self.total = self._hard_total
+
+
 if __name__ == '__main__':
     d = Deck()
     h = Hand_Lazy(d.pop(), d.pop(), d.pop())
     print(h.total)
     h.card = d.pop()
     print(h.total)
+
+
+    d = Deck()
+    h = Hand_Eager(d.pop(), d.pop(), d.pop())
+    print(h.total)
+    h.cards = d.pop()
+    print(h.total)
+
+    d = Deck()
+    c = d.pop()
+    h = Hand_Lazy(d.pop(), c, c)  # Force splittable hand
+    h2 = h.split(d)
+
+    print(h)
+    print(h2)
