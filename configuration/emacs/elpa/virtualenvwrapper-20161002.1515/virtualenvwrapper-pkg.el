a964d70d36c23713df958ee7d51d3bf8c2a5cfbd
Asher Guan <python.sth@gmail.com>
2018-01-04 18:10:23 +0800

    del emacs config file

--- configuration/emacs/elpa/virtualenvwrapper-20161002.1515/virtualenvwrapper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "virtualenvwrapper" "20161002.1515" "a featureful virtualenv tool for Emacs" '((dash "1.5.0") (s "1.6.1")) :commit "15af07004dba640df8202e76bea0b5dc82567f72" :url "http://github.com/porterjamesj/virtualenvwrapper.el" :keywords '("python" "virtualenv" "virtualenvwrapper"))
