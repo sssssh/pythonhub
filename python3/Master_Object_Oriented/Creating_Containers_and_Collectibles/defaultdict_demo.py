8270556c5a5c499cb807559c606f197e2e8ee480
Asher Guan <python.sth@gmail.com>
2018-01-05 23:40:37 +0800

    del all

--- python3/Master_Object_Oriented/Creating_Containers_and_Collectibles/defaultdict_demo.py
+++
@@ -1,9 +0,0 @@
-from collections import defaultdict
-
-
-messages = defaultdict(lambda: "N/A")
-messages['error'] = 'Full Error Text'
-messages['other']
-
-
-used_default= [k for k in messages if messages[k] == "N/A"]
